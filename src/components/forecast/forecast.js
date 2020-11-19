f4c0b5ddf93abf7ca1840d6acbb0f1eb9a21856f
marsha <marshaannon@gmail.com>
2020-11-19 18:34:23 -0500

    add ability to retrieve data from any us city and in celsius or fahrenheit

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import Conditions from '../Conditions/Conditions';
-import ForecastForm from './ForecastForm';
 
 const Forecast = () => {
+  let [ city, setCity ] = useState("");
+  let [ unit, setUnit ] = useState("imperial");
   let [ resObj, setResObj ] = useState({});
 
-  function getForecast() {
-    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=New%20York`, {
+  const uriEncodedCity = encodeURIComponent(city);
+
+  function getForecast(e) {
+    e.preventDefault();
+    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
 	    "method": "GET",
 	    "headers": {
 		    "x-rapidapi-key": "5e07d5ec86mshaec91845041a3b4p133e69jsncae261294a03",
@@ -27,7 +31,34 @@ const Forecast = () => {
         <div>
           {JSON.stringify(resObj)}
         </div>
-        < ForecastForm />
+        <form onSubmit={ getForecast }>
+          <input
+            type="text"
+            placeholder="Enter City"
+            maxLength="50"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <label>
+            <input
+              type="radio"
+              name="units"
+              checked={unit === "imperial"}
+              value="imperial"
+              onChange={(e) => setUnit(e.target.value)}
+            /> Fahrenheit
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="units"
+              checked={unit === "metric"}
+              value="metric"
+              onChange={(e) => setUnit(e.target.value)}
+            /> Celsius
+          </label>
+          <button type="submit">Get Forecast</button>
+        </form>
         < Conditions resObj={resObj} />
     </div>
   )
