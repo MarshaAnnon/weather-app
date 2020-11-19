ffca424d5868f0d7b4005fd8c226f432fe9ac9c5
marsha <marshaannon@gmail.com>
2020-11-19 16:49:43 -0500

    fix fetch url

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,34 +1,31 @@
 import React, { useState } from 'react';
 
 const Forecast = () => {
-  let [ resObject, setResObject ] = useState({});
+  let [ resObj, setResObj ] = useState({});
 
   function getForecast() {
-    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=newyork,ny", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "5e07d5ec86mshaec91845041a3b4p133e69jsncae261294a03",
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-	}
-})
-.then(res => res.json())
-.then(res ={
-  setResObject(res)
-})
-.catch(err => {
-	console.error(err);
-});
+    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=New%20York", {
+	    "method": "GET",
+	    "headers": {
+		    "x-rapidapi-key": "5e07d5ec86mshaec91845041a3b4p133e69jsncae261294a03",
+		    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	    }
+    })
+    .then(res => res.json())
+    .then(res => {
+      setResObj(res)
+    })
+    .catch(err => {
+	    console.error(err);
+    })
   }
-
   return (
     <div>
       <h2>Find Current Weather Conditions</h2>
-      <div>
-        {JSON.stringify(resObj)}
-      </div>
-      <button onClick={ getForecast }>
-        Get Forecast
-      </button>
+        <div>
+          {JSON.stringify(resObj)}
+        </div>
+        <button onClick={getForecast}>Get Forecast</button>
     </div>
   )
 }
