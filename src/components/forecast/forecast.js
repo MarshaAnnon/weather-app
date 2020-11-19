52c97ec02d1019a3f32007764814d75b85062e5f
marsha <marshaannon@gmail.com>
2020-11-19 14:12:10 -0500

    add Forecast function with the function a get request to the weather api, also add initial jsx code to display information

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Forecast = () => {
+  function getForecast() {
+    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=newyork,ny", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "5e07d5ec86mshaec91845041a3b4p133e69jsncae261294a03",
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	}
+})
+.then(res => {
+	console.log(res);
+})
+.catch(err => {
+	console.error(err);
+});
+  }
+
+  return (
+    <div>
+      <h2>Find Current Weather Conditions</h2>
+      <div>
+        {JSON.stringify(resObj)}
+      </div>
+      <button onClick={ getForecast }>
+        Get Forecast
+      </button>
+    </div>
+  )
+}
+
+export default Forecast;
