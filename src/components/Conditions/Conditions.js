6ed2f1c10ad87eb5e213344a2439d6958d5f9cf4
marsha <marshaannon@gmail.com>
2020-11-19 19:28:09 -0500

    import Conditions.modules.css and add classNames to div

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import classes from './Conditions.module.css';
+
  
 const Conditions = (props) => {
   return (
     <div>
       { props.resObj.cod === 200 ?
-        <div>
+        <div className={classes.wrapper}>
           <p><strong>{ props.resObj.name }</strong></p>
           <p>It's currently { Math.round(props.resObj.main.temp) } degrees with { props.resObj.weather[0].description }.</p>
           <p>It feels like { Math.round(props.resObj.main.feels_like)} degrees</p>
-          <p>Humidity is { props.resObj.main.humidity}</p>
+          <p>Humidity is { props.resObj.main.humidity}%</p>
         </div>
         : null
       }
